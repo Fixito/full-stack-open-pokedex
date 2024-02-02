d076fd6a400d527ca49b95585fc60ae549042804
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-02-02 14:05:41 +0100

    Add cypress + github worflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     es6: true,
     'jest/globals': true,
     node: true,
+    'cypress/globals': true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
@@ -18,7 +19,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'jest'],
+  plugins: ['react', 'jest', 'cypress'],
   rules: {
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
